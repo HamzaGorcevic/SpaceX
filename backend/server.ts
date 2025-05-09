ceb7e32c878b8f7a4292615e2360253833d64f08
HamzaGorcevic <gorcevichamza76@gmail.com>
2025-05-09 15:07:42 +0200

    productiong configuration

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,7 @@
 import app from "./src/app"
 import {connectToMongoDB} from "./src/db/db"
+import dotenv from "dotenv"
+dotenv.config()
 const PORT = 3000
 
 const startServer = async()=>{
--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToMongoDB =async()=>{
     try{
-        await mongoose.connect("mongodb://localhost:27017/mern")
+        await mongoose.connect(process.env.CONNECTION_STRING||"")
         console.log("Successfully connected");
         
     }catch(err){
